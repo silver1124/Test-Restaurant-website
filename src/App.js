35ce6eb735195dd8a0206accc6959228e32c03a9
silver1124 <136788502+silver1124@users.noreply.github.com>
2023-08-09 10:19:06 +0000

    Implementing the cart Itemns page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { Fragment, useState } from "react";
-import Header from "./Components/Layout/Header";
-import Meals from "./Components/Meals/Meals";
-import Cart from "./Components/Cart/Cart";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
   };
+
   const hideCartHandler = () => {
-    setCartIsShown(false)
+    setCartIsShown(false);
   };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,43 +1,50 @@
-import classes from "./AvailableMeals.module.css";
-import Card from "../UI/Card";
-import MealItem from "./MealItems/MealItem";
+import Card from '../UI/Card';
+import MealItem from './MealItems/MealItem';
+import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
   {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
     price: 22.99,
   },
   {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
     price: 16.5,
   },
   {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
     price: 12.99,
   },
   {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
     price: 18.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meals) => 
-  <MealItem key={meals.id} name={meals.name} description={meals.description} price={meals.price} />);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>
-        <Card>{mealsList}</Card>
-      </ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
